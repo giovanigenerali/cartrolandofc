98167c3f46eb09c210beb1609afea643395d3a09
Giovani Generali <giovani.generali@gmail.com>
2016-05-21 00:45:40 -0300

    load api using curl

--- load-api.php
+++ load-api.php
@@ -1,18 +1,20 @@
 <?php
-
   header('Content-type: application/json');
 
   if (isset($_GET["api"]) and $_GET["api"] !== "") {
 
     if ($_GET["api"] === "busca-time") {
-
-      echo file_get_contents("https://api.cartolafc.globo.com/times?q=". rawurlencode($_GET["team"]));
-
+      $url = "https://api.cartolafc.globo.com/times?q=". rawurlencode($_GET["team"]);
     } else if ($_GET["api"] === "busca-atletas") {
-
-      echo file_get_contents("https://api.cartolafc.globo.com/time/". $_GET["team_slug"]);
-
+      $url = "https://api.cartolafc.globo.com/time/". $_GET["team_slug"];
     }
 
+    $c = curl_init();
+    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($c, CURLOPT_URL, $url);
+    $result = curl_exec($c);
+    curl_close($c);
+    echo $result;
   }
 ?>
