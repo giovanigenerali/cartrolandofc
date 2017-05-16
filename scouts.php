e16a810560779382c8e7fad87eabbd872a7c1f74
Giovani Generali <giovani.generali@gmail.com>
2017-05-15 23:51:04 -0300

    scouts
    relação de scouts com sigla, descrição da ação e pontos.

---
+++ scouts.php
@@ -0,0 +1,77 @@
+<?php
+"scouts" => array(
+  "RB" => array(
+    "acao" => "Roubada de bola",
+    "pontos" => 1.7
+  ),
+  "FC" => array(
+    "acao" => "Falta cometida",
+    "pontos" => -0.5
+  ),
+  "GC" => array(
+    "acao" => "Gol contra",
+    "pontos" => -6.0
+  ),
+  "CA" => array(
+    "acao" => "Cartão amarelo",
+    "pontos" => -2.0
+  ),
+  "CV" => array(
+    "acao" => "Cartão vermelho",
+    "pontos" => -5.0
+  ),
+  "FS" => array(
+    "acao" => "Falta sofrida",
+    "pontos" => 0.5
+  ),
+  "PE" => array(
+    "acao" => "Passe errado",
+    "pontos" => -0.3
+  ),
+  "FT" => array(
+    "acao" => "Finalização na trave",
+    "pontos" => 3.5
+  ),
+  "FD" => array(
+    "acao" => "Finalização defendida",
+    "pontos" => 1.0
+  ),
+  "FF" => array(
+    "acao" => "Finalização para fora",
+    "pontos" => 0.7
+  ),
+  "G" => array(
+    "acao" => "Gols",
+    "pontos" => 8.0
+  ),
+  "I" => array(
+    "acao" => "Impedimento",
+    "pontos" => -0.5,
+    "scout" => ""
+  ),
+  "PP" => array(
+    "acao" => "Penalti perdido",
+    "pontos" => -3.5
+  ),
+  "A" => array(
+    "acao" => "Assistência",
+    "pontos" => 5.0
+  ),
+  "SG" => array(
+    "acao" => "Jogo sem sofrer gol",
+    "pontos" => 5.0
+  ),
+  "DD" => array(
+    "acao" => "Defesa difícil",
+    "pontos" => 3.0
+  ),
+  "DP" => array(
+    "acao" => "Defesa de penalti",
+    "pontos" => 7.0
+  ),
+  "GS" => array(
+    "acao" => "Gol sofrido",
+    "pontos" => -2.0
+  )
+);
+ ?>
